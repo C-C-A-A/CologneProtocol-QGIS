cc9cda8b8c718c679200089f844ccb52e43204cb
RobinCGN <mail2rp@gmx.net>
2020-04-15 16:32:15 +0200

    Changed order of parameters

--- Models/03_Kriging.py
+++ Models/03_Kriging.py
@@ -15,16 +15,16 @@ class _kriging(QgsProcessingAlgorithm):
 
     # Initalize input parameters
     def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterVectorLayer('sites', 'a_LEC', types=[QgsProcessing.TypeVectorPoint], defaultValue=None))
+        self.addParameter(QgsProcessingParameterField('hubdistlecradii', 'HubDist_LEC_Radii', type=QgsProcessingParameterField.Numeric, parentLayerParameterName='sites', allowMultiple=False, defaultValue='HubDist'))
+        self.addParameter(QgsProcessingParameterNumber('gridcellsize', 'Grid_Cellsize', type=QgsProcessingParameterNumber.Integer, defaultValue=1000))
+        self.addParameter(QgsProcessingParameterExpression('formel', 'Formel', parentLayerParameterName='', defaultValue=''))
+        self.addParameter(QgsProcessingParameterNumber('lagdist', 'LagDist', type=QgsProcessingParameterNumber.Double, defaultValue=0))
         self.addParameter(QgsProcessingParameterBoolean('BlockKriging', 'Block_Kriging', defaultValue=True))
         self.addParameter(QgsProcessingParameterNumber('blocksize', 'Block_Size', type=QgsProcessingParameterNumber.Integer, defaultValue=100))
-        self.addParameter(QgsProcessingParameterExpression('formel', 'Formel', parentLayerParameterName='', defaultValue=''))
-        self.addParameter(QgsProcessingParameterNumber('gridcellsize', 'Grid_Cellsize', type=QgsProcessingParameterNumber.Integer, defaultValue=1000))
-        self.addParameter(QgsProcessingParameterField('hubdistlecradii', 'HubDist_LEC_Radii', type=QgsProcessingParameterField.Numeric, parentLayerParameterName='sites', allowMultiple=False, defaultValue='HubDist'))
-        self.addParameter(QgsProcessingParameterNumber('lagdist', 'LagDist', type=QgsProcessingParameterNumber.Double, defaultValue=0))
         self.addParameter(QgsProcessingParameterNumber('maxsearchdist', 'Max_Search_Dist', type=QgsProcessingParameterNumber.Integer, defaultValue=""))
-        self.addParameter(QgsProcessingParameterNumber('numberofptsmax', 'Number_of_Pts_Max', type=QgsProcessingParameterNumber.Integer, defaultValue=10))
         self.addParameter(QgsProcessingParameterNumber('numberofptsmin', 'Number_of_Pts_Min', type=QgsProcessingParameterNumber.Integer, defaultValue=3))
-        self.addParameter(QgsProcessingParameterVectorLayer('sites', 'a_LEC', types=[QgsProcessing.TypeVectorPoint], defaultValue=None))
+        self.addParameter(QgsProcessingParameterNumber('numberofptsmax', 'Number_of_Pts_Max', type=QgsProcessingParameterNumber.Integer, defaultValue=10))
         self.addParameter(QgsProcessingParameterRasterDestination('Kriging_raster', 'Kriging_Raster', createByDefault=True, defaultValue="Kriging.sdat"))
         self.addParameter(QgsProcessingParameterRasterDestination('Quality_measure', 'Quality_Measure', optional=True, createByDefault=False, defaultValue="Variance.sdat"))
 
